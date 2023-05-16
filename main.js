1b38f5e280d8c75e9fb0115dbe359ba22c1a2525
Jonathan Sánchez <jonulisanpar@gmail.com>
2023-05-16 14:31:19 -0600

    Mas tiles, no funcionan xd

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+
+const attributionControl = new ol.control.Attribution({
+    collapsible: true
+})
+
 const init = () => {
 
     // Create a map object
@@ -23,7 +28,7 @@ const init = () => {
             new ol.layer.Tile({
                 source: new ol.source.OSM(), // OSM -> OpenStreetMap
                 zIndex: 1,
-                visible: false,
+                visible: true,
                 /*
                 extent: [Xmin, Ymin, Xmax, Ymax]
 
@@ -32,17 +37,19 @@ const init = () => {
                 Si existe una capa inferior, se logra un efecto de mapa diferente
                 para esa seccion
                 */
-                extent: [
-                    -13424929.347505514, 
-                    1287199.7496701013, 
-                    -9535813.348355746, 
-                    3831024.0510007674
-                ],
-                opacity: 0.5
+                // extent: [
+                //     -13424929.347505514, 
+                //     1287199.7496701013, 
+                //     -9535813.348355746, 
+                //     3831024.0510007674
+                // ],
+                // opacity: 0.5
+                opacity: 1
             })
         ],
         // Target
         target: "js-map",
+        controls: ol.control.defaults({attribution: false}).extend([attributionControl])
     });
 
     // Layer Group
@@ -68,7 +75,7 @@ const init = () => {
                     key: 'ArAgLEHRfl7WKapNubsPQf6XKRjKdfFzTOc4sAUwkAheJikVlCnCloTXmNjDeYVn',
                     imagerySet: 'AerialWithLabels', // Aerial, AerialWithLabels, Road, CanvasDark, CanvasGray
                 }),
-                visible: false
+                visible: true
             })
         ]
     })
@@ -79,8 +86,9 @@ const init = () => {
     const cartoDBBaseLayer = new ol.layer.Tile({
         source: new ol.source.XYZ({
             url: 'https://{1-4}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}{scale}.png',
+            attributions: '@ CARTO'
         }),
-        visible: false
+        visible: true
     });
 
     map.addLayer(cartoDBBaseLayer);
@@ -96,21 +104,46 @@ const init = () => {
     // Stamen basemap Layer
     const stamenBaseLayer = new ol.layer.Tile({
         source: new ol.source.Stamen({
-            layer: 'terrain-labels'
+            layer: 'terrain-labels',
+            attributions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
         }),
-        visible: false
+        visible: true
     });
 
     map.addLayer(stamenBaseLayer)
 
     const stamenBaseMapLayer = new ol.layer.Tile({
         source: new ol.source.XYZ({
-            url: 'https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png'
+            url: 'https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png',
+            attributions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
         }),
-        visible: true
+        visible: false
     })
 
     map.addLayer(stamenBaseMapLayer)
+
+    // tile ArcGIS REST API Layer (NO FUNCIONA)
+//   const tileArcGISLayer = new ol.layer.Tile({
+//     source: new ol.source.TileArcGISRest({
+//       url: "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Louisville/LOJIC_LandRecords_Louisville/MapServer"
+//     }),
+//     visible: true
+//   })
+//   map.addLayer(tileArcGISLayer);
+
+    // NOAA WMS Layer (NO FUNCIONA)
+    // const NOAAWMSLayer = new ol.layer.Tile({
+    //     source: new ol.source.TileWMS({
+    //         url: 'https://nowcoast.noaa.gov/arcgis/services/nowcoast/forecast_meteoceanhydro_sfc_ndfd_dailymaxairtemp_offsets/MapServer/WMSServer?',
+    //         params: {
+    //             LAYERS: 5,
+    //             FORMAT: 'image/png',
+    //             TRANSPARENT: true
+    //         },
+    //         attributions: '<a href=https://nowcoast.noaa.gov/>© NOAA<a/>'
+    //     })
+    // })
+    // map.addLayer(NOAAWMSLayer);
 
     // map.on('click', (e) => {
     //     console.log(e.coordinate);
